d59b32e62b335da49d57474a22ebfed3df8d5654
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 21:24:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-remove_element_from_end_of_array = create_an_array.pop
+remove_element_from_end_of_array = add_element_to_start_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
