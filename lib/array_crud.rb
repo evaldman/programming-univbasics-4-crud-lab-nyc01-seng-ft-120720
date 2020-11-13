3ea1c257d60e88b60d2f7a0293a55e2f76a96b81
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 21:09:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element) 
-  ["cliff", "jeff"]
+  add_element_to_end_of_array = create_an_array
   add_element_to_end_of_array.push("steve")
 end
 
