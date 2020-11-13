bb8bb58e82d0a801033599fda8d5051e39ad5bb6
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 21:12:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element) 
   
-  create_an_array.push("steve")
+  create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
