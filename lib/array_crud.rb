d7404d58ef8376a454d3a3d8a9e73e21e991467e
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 21:38:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  create_an_array
 end
 
 def update_element_from_index(array, index_number, element)
