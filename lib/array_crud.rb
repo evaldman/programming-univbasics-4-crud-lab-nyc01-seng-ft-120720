aae861e19e9216e765b1711a24faa2be97046a59
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 21:33:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   create_an_array = ["bob", "rob", "ted", "fred", "arrays!"]
+   create_an_array = ["wow","bob", "rob", "ted", "fred", "arrays!"]
+   create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
